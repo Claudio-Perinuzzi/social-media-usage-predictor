a99eea3ec878de4a801734252d139289a23821ce
Claudio P <perinuzzic@gmail.com>
2024-09-11 10:31:48 -0400

    cleaned main functions

--- src/Main.java
+++ src/Main.java
@@ -14,81 +14,13 @@ import java.util.Arrays;
     // jar cfm dist/predict.jar META-INF/MANIFEST.MF -C bin .
 
 //NEXT STEPS:
-    //option for a serialized model
+    //offer helpful suggestions
+    //option for a serialized model 
     //another option for not serializing the model
-    //clean below
     //clean all files
-    //images for readme, including sequence diagrams (create assets folder)
+    //images for readme and finalize readme
 
 public class Main {
-
-    public static void ensureSerializedModel(RandomForest forest) {
-        String modelPath = "model/randomForestModel.ser";
-        File file = new File(modelPath);
-
-        if (file.exists()) {
-            //load in
-            
-            // Deserialize the trained forest
-            try (ObjectInputStream ois = new ObjectInputStream(new GZIPInputStream(new FileInputStream(modelPath)))) {
-                forest = (RandomForest) ois.readObject();
-                System.out.println("Random Forest model deserialized successfully.");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-    
-        }
-        else {
-            //train and save
-            forest = new RandomForest();
-            forest.train(100);
-
-            //serialize the trained forest
-            try (ObjectOutputStream oos = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(modelPath)))) {
-                oos.writeObject(forest);
-                System.out.println("Random Forest model serialized successfully.");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-
-        }    
-    }
-
-
-    
-    // IP ##########################################
-    // Determine the outcome by running the user's input against the forest model and counting the predictions  
-    public static void predict(RandomForest forest, DataContainer userInput) {
-        int[] predictions = forest.aggregate(userInput); // Get predictions from the forest
-        int yes = 0, no = 0; // Initialize label counts for voting
-        
-        // Count the yes and no labels
-        for (int i = 0; i < predictions.length; i++) {
-            if (predictions[i] == 1) yes++;
-            else no++;
-        }
-
-        if (yes > no) {
-            System.out.println("You are addictied");
-            System.out.println("Confidence is " + yes);
-        }
-        else {
-            System.out.println("You are not addicted");
-            System.out.println("Yes " + yes);
-            System.out.println("No " + no);
-        }
-   
-    }
-
-
-    // Train the model with 100 decision trees
-    public static void trainNewModel(RandomForest forest) {
-        forest = new RandomForest();
-        forest.train(100);    
-    }
-
-
     public static void main(String[] args)  {
         
         // // Get the first arg index flag on whether the user wants to use the serialized model 
@@ -109,7 +41,7 @@ public class Main {
         // }
         // // Or, train a new model into the null forest
         // else { 
-        //     trainNewModel(forest);
+        //     trainNewModel(forest, 100);
         // }
 
         // //get the prediction using the forest and the user's input.
@@ -118,8 +50,79 @@ public class Main {
         //TESTING/////////////////////////////////////////////////////////////////
         String input = ",,2,,,,,,,,,,";
         DataContainer userInput = new DataContainer(input);
-        userInput.print();
+        // userInput.print();
         RandomForest forest = new RandomForest(100);
         predict(forest, userInput);
+        //TESTING/////////////////////////////////////////////////////////////////
     }
+
+
+
+
+    // Determine the outcome by running the user's input against the forest model and counting the predictions  
+    public static void predict(RandomForest forest, DataContainer userInput) {
+        
+        // Get predictions from the forest and initialize label counts for voting
+        int[] predictions = forest.aggregate(userInput); 
+        int yes = 0, no = 0; 
+        
+        // Count the yes and no labels
+        for (int i = 0; i < predictions.length; i++) {
+            if (predictions[i] == 1) yes++;
+            else no++;
+        }
+        
+        // Calculate confidence level for each label
+        double yesConfidence = (double) yes / (yes + no);
+        double noConfidence = (double) no / (yes + no);
+        
+        // Print whether the user may be addicted to social media and offer suggestions
+        if (yes > no) {
+            System.out.println("You may be at risk of social media addiction!");
+            System.out.println("Confidence Level: " + String.format("%.2f", yesConfidence * 100) + "%");
+        } else {
+            System.out.println("Your social media usage is within a healthy range!");
+            System.out.println("Confidence Level: " + String.format("%.2f", noConfidence * 100) + "%");
+        }
+    }
+
+
+    // Train the model with 100 decision trees
+    public static void trainNewModel(RandomForest forest, int numTrees) {
+        forest = new RandomForest();
+        forest.train(numTrees);    
+    }
+
+
+    // Ensure that the serialized model exists. If not, train a new model and save it
+    public static void ensureSerializedModel(RandomForest forest) {
+        
+        // Path the model exists and a create new file object at that path
+        String modelPath = "model/randomForestModel.ser";
+        File file = new File(modelPath);
+
+        
+        if (file.exists()) { // Load in the model if it exists
+            // Deserialize the trained forest and read it into the forest object
+            try (ObjectInputStream ois = new ObjectInputStream(new GZIPInputStream(new FileInputStream(modelPath)))) {
+                forest = (RandomForest) ois.readObject();
+                System.out.println("Random Forest model deserialized successfully.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        else { // Train and save a new model if it does not exist in the file path
+            trainNewModel(forest, 100);
+            //serialize the trained forest and save it to the file path
+            try (ObjectOutputStream oos = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(modelPath)))) {
+                oos.writeObject(forest);
+                System.out.println("Random Forest model serialized successfully.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }    
+    }
+
+
 }
