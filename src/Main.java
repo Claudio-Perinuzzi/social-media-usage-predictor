dfc8d5afdd89dd70766f58ca0d1147de6cd7d997
Claudio P <perinuzzic@gmail.com>
2024-10-09 23:30:38 -0400

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,47 @@
-import java.io.File;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.GZIPInputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-import java.util.zip.GZIPOutputStream;
-import java.util.zip.GZIPInputStream;
 import java.util.Arrays;
+import java.io.File;
 
-
-//WHEN READY TO DEPLOY
-    // javac --release 17 src/*.java
-    // jar cfm dist/predict.jar META-INF/MANIFEST.MF -C bin .
-
-//NEXT STEPS:
-    //offer helpful suggestions
-    //option for a serialized model 
-    //another option for not serializing the model
-    //clean all files
-    //images for readme and finalize readme
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * MAIN
+ * Collects a command line argument used to either load in or train a new random forest model
+ * which is then used to print back to the service whether the user is at risk of social
+ * media addiction and what the corresponding confidence score is.
+ * 
+ * Deployment:
+ *      javac --release 17 src/*.java
+ *      jar cfm dist/predict.jar META-INF/MANIFEST.MF -C bin .
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 public class Main {
     public static void main(String[] args)  {
         
         // Get the arg index 0 flag on whether the user wants to use the serialized model 
         boolean toSerialize = (args[0].equals("True")); 
-        String location = args[1];
-        String hobbies = args[2];
 
-        // Get the rest of the arguments
+        // Get the rest of the users input arguments
         String input = String.join(",", Arrays.copyOfRange(args, 3, args.length)); 
 
         // Create a DataContainer object of the user's input 
         DataContainer userInput = new DataContainer(input); 
 
-                    System.out.print("to serialize = ");
-                    System.out.println(toSerialize);
-                    System.out.println("my location is " + location);
-                    System.out.println("my hobbies are " + hobbies);
-                    System.out.println("my input is " + input);
-                    System.out.print("my data container input is: ");
-                    userInput.print();
-
-                    
-
-
-        // Generate a null forest that is ready to be either trained or loaded into
-        // RandomForest forest = null;
-
-        // Load the serialized model into the forest
+        // Load the serialized model into the forest and predict
         if (toSerialize) {
             RandomForest forest = ensureSerializedModel();     
             predict(forest, userInput);
         }
-        // Or, train a new model into the null forest
+        // Or, train a new model with 100 trees and predict 
         else { 
             RandomForest forest = trainNewModel(100);
             predict(forest, userInput);
         }
-        
-        //get the prediction using the forest and the user's input.
-        // predict(forest, userInput);
 
-        //TESTING/////////////////////////////////////////////////////////////////
-        // String input = ",,2,,,,,,,,,,";
-        // DataContainer userInput = new DataContainer(input);
-        // // userInput.print();
-        // RandomForest forest = new RandomForest(100);
-        // predict(forest, userInput);
-        //TESTING/////////////////////////////////////////////////////////////////
     }
-
-
-
 
     // Determine the outcome by running the user's input against the forest model and counting the predictions  
     public static void predict(RandomForest forest, DataContainer userInput) {
@@ -86,22 +56,21 @@ public class Main {
             else no++;
         }
         
-        // Calculate confidence level for each label
-        double yesConfidence = (double) yes / (yes + no);
-        double noConfidence = (double) no / (yes + no);
+        // Calculate probability of addiction
+        double probability = (double) yes / (yes + no);
         
         // Print whether the user may be addicted to social media and offer suggestions
         if (yes > no) {
             System.out.println("You may be at risk of social media addiction!");
-            System.out.println("Confidence Level: " + String.format("%.2f", yesConfidence * 100) + "%");
+            System.out.println("Probability of addiction: " + String.format("%.0f", probability * 100) + "%");
         } else {
             System.out.println("Your social media usage is within a healthy range!");
-            System.out.println("Confidence Level: " + String.format("%.2f", noConfidence * 100) + "%");
+            System.out.println("Probability of addiction: " + String.format("%.0f", probability * 100) + "%");
         }
     }
 
 
-    // Train the model with 100 decision trees
+    // Train the model with n decision trees
     public static RandomForest trainNewModel(int numTrees) {
         RandomForest forest = new RandomForest();
         forest.train(numTrees);   
